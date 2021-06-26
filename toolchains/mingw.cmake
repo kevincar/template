57e16dbf8b354b60ee7ae17b841ffd5b532789f8
Kevin Davis <kevincarrolldavis@gmail.com>
2021-06-25 21:57:58 -0600

    static linking

--- toolchains/mingw.cmake
+++ toolchains/mingw.cmake
@@ -15,3 +15,5 @@ SET(CMAKE_FIND_ROOT_PATH  /usr/local/Cellar/mingw-w64/6.0.0_1)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++ -static-libgcc -static -lpthread")
